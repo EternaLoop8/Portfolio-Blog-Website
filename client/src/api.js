639cdebf2eb6cad4c928f9191bf91626d985cfc6
EternaLoop8 <soumyaperso06@gmail.com>
2026-02-10 00:04:44 +0530

    Added new baseURL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://portfolio-blog-website-5m1i.onrender.com/api",
 });
 
 // Add JWT automatically to all requests
