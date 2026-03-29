e9aa9e0b13786658c7d39770ef1424ec8da5d8a3
EternaLoop8 <soumyaperso06@gmail.com>
2026-03-29 23:11:11 +0530

    Update base URI

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://portfolio-blog-website-lgpj.vercel.app/api",
 });
 
 // Add JWT automatically to all requests
