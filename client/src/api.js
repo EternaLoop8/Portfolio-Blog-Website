3f12e78d8608352efe8b2ba301ae371e2f5547a7
EternaLoop8 <soumyaperso06@gmail.com>
2026-03-29 23:25:47 +0530

    New backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://portfolio-blog-website-lgpj.vercel.app/api",
+  baseURL: "https://portfolio-blog-website-b2bc.vercel.app/api",
 });
 
 // Add JWT automatically to all requests
